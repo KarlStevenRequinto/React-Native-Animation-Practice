f9f319e76ad78e2ee881f8a55d4254af162478d7
KarlStevenRequinto <lerdrobott@gmail.com>
2023-01-11 05:36:55 +0800

    phone ring animations using motiview

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { MotiView, MotiText } from "moti";
+import { Easing } from "react-native-reanimated";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
+      <View style={[styles.dot, styles.center]}>
+        {[...Array(3).keys()].map((index) => {
+          return (
+            <MotiView
+              key={index}
+              style={[StyleSheet.absoluteFillObject, styles.dot]}
+              from={{ opacity:0.7, scale: 1 }}
+              animate={{ opacity: 0, scale: 4 }}
+              transition={{
+                type: "timing",
+                duration: 2000,
+                easing: Easing.out(Easing.ease),
+                delay: index * 300,
+                repeatReverse:false,
+                loop: true,
+              }}
+            />
+          );
+        })}
+        <Feather name="phone-outgoing" size={32} color="#fff" />
+      </View>
     </View>
   );
 }
@@ -13,6 +36,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dot: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+    backgroundColor: "#6e01ef",
+  },
+  center: {
     alignItems: "center",
     justifyContent: "center",
   },
